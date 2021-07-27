663c80093e39dcc9c08358511a8997ebdd5edf58
Bhavik Bhalodia <20952138+babhalodia@users.noreply.github.com>
2021-07-27 16:17:06 +0530

    Initial Commit

---
+++ TA-microsoft-graph-security-score-add-on-for-splunk/README/inputs.conf.spec
@@ -0,0 +1,4 @@
+[graph_security_score_input://<name>]
+azure_ad_tenant_id = 
+application_id = 
+client_secret =
---
+++ TA-microsoft-graph-security-score-add-on-for-splunk/bin/TA_microsoft_graph_security_score_add_on_for_splunk_rh_graph_security_score_input.py
@@ -0,0 +1,88 @@
+
+import ta_microsoft_graph_security_score_add_on_for_splunk_declare
+
+from splunktaucclib.rest_handler.endpoint import (
+    field,
+    validator,
+    RestModel,
+    DataInputModel,
+)
+from splunktaucclib.rest_handler import admin_external, util
+from splunk_aoblib.rest_migration import ConfigMigrationHandler
+
+util.remove_http_proxy_env_vars()
+
+
+fields = [
+    field.RestField(
+        'interval',
+        required=True,
+        encrypted=False,
+        default=None,
+        validator=validator.Pattern(
+            regex=r"""^\-[1-9]\d*$|^\d*$""", 
+        )
+    ), 
+    field.RestField(
+        'index',
+        required=True,
+        encrypted=False,
+        default='default',
+        validator=validator.String(
+            min_len=1, 
+            max_len=80, 
+        )
+    ), 
+    field.RestField(
+        'azure_ad_tenant_id',
+        required=True,
+        encrypted=False,
+        default=None,
+        validator=validator.String(
+            min_len=0, 
+            max_len=8192, 
+        )
+    ), 
+    field.RestField(
+        'application_id',
+        required=True,
+        encrypted=False,
+        default=None,
+        validator=validator.String(
+            min_len=0, 
+            max_len=8192, 
+        )
+    ), 
+    field.RestField(
+        'client_secret',
+        required=True,
+        encrypted=True,
+        default=None,
+        validator=validator.String(
+            min_len=0, 
+            max_len=8192, 
+        )
+    ), 
+
+    field.RestField(
+        'disabled',
+        required=False,
+        validator=None
+    )
+
+]
+model = RestModel(fields, name=None)
+
+
+
+endpoint = DataInputModel(
+    'graph_security_score_input',
+    model,
+)
+
+
+if __name__ == '__main__':
+    admin_external.handle(
+        endpoint,
+        handler=ConfigMigrationHandler,
+    )
